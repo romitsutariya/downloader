a34a04221f9d39b86c2d8d9788861432d2809779
romitsutariya <romitsutariya@gmail.com>
2019-03-10 12:25:30 +0530

    Signed-off-by: romitsutariya <romitsutariya@gmail.com>

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors')
-var MongoClient = require('mongodb').MongoClient;
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static("public/"));
-
- //this is for  loading first page   
- app.get('/', function (req, res) {
-    res.send("hello world");
- })
-
-const port = process.env.PORT || 3000;
-//server configuration file
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-})
+const TelegramBot = require('node-telegram-bot-api');
+const token = '728281689:AAHRrCd2ISJty0Q532jGGTNQNJEOoc9JO0Y';
+const bot = new TelegramBot(token, {polling: true})
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Ill have the tuna. No crust.');
+});
