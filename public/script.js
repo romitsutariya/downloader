7ce7c88a31864896090e44f61cd7692efe0d2a12
romitsutariya <romitsutariya@gmail.com>
2019-06-02 14:36:28 +0530

    create ui from downloader

---
+++ public/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    var convertBtn = document.querySelector('.convert-button');
+    var URLinput = document.querySelector('.URL-input');
+    convertBtn.addEventListener('click', () => {
+        console.log(`URL: ${URLinput.value}`);
+        sendURL(URLinput.value);
+    });
+    function sendURL(URL) {
+        // We will put code here later
+    }
+});
