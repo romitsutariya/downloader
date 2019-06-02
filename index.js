1ca6c75343cd5309bb0e7d103001e48baed6bfeb
romitsutariya <romitsutariya@gmail.com>
2019-06-02 11:29:52 +0530

    init for  downloader

--- index.js
+++
@@ -1,6 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '728281689:AAHRrCd2ISJty0Q532jGGTNQNJEOoc9JO0Y';
-const bot = new TelegramBot(token, {polling: true})
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Ill have the tuna. No crust.');
-});
